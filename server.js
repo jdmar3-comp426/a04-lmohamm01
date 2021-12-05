1e9268913831249468d1b7e33d0020fb2fdf978c
Lidia Mohammed <lidiamohammed@Lidias-MBP.lan>
2021-12-04 19:21:37 -0500

    f

--- server.js
+++ server.js
@@ -53,13 +53,10 @@ app.get("/app/user/:id", (req, res) => {
 app.patch("/app/update/user/:id", (req, res) => {
     const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?, pass) WHERE id = ?").all();
     const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
-    res.json({ "message": "1 record updated: ID " + req.params.id + " (200)" });
-    res.status(201);
+    res.status(201).json({ "message": "1 record updated: ID " + req.params.id + " (200)" });
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-const stmt = db.prepare("SELECT * FROM userinfo").all();
-
 app.delete("/app/delete/user/:id", (req, res) => {
     const stmt = db.prepare("DELETE DROM userinfo where id = ?").run(req.params.id);
     res.json({ "message": "1 record deleted: ID " + req.params.id + " (200)" });
